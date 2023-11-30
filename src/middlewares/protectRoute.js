a82ef8efde0a176baf0a2889ac718480902e72ca
Haziel Ortiz <ya-yo22124@outlook.com>
2023-11-30 12:42:26 -0600

    Upd

--- src/middlewares/protectRoute.js
+++ src/middlewares/protectRoute.js
@@ -1,5 +1,20 @@
 const protectRoute = async (req, res, next) => {
     console.log("Hola desde middleware");
+    // TODO: Verificar si hay un tokem
+    const { _token } = req.cookies;
+
+    if (!_token) {
+        // TODO: Verificar el token
+        return res.redirect("/bienes-raices/user/login")
+    }
+
+    try {
+
+    } catch (error) {
+
+        return res.clearCookie("_token").redirect("/bienes-raices/user/login")
+    }
+
     next();
 }
 
